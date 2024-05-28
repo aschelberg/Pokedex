3c5c8222fe8c1da0192719d69c1644711babce38
aschelberg <39310235+aschelberg@users.noreply.github.com>
2024-05-28 16:16:34 -0400

    Installed firebase

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import "@/assets/styles.css";
-
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-
+import '@/assets/styles.css';
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { initializeApp } from 'firebase/app';
 import { createEarthoOne } from "@eartho/one-client-vue";
 
-import App from "./App.vue";
-import router from "./router";
+import App from './App.vue';
+import router from './router';
 
 const app = createApp(App);
 
